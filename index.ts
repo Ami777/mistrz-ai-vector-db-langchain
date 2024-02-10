7f3d71bb06f799e9eab1e3e169ec1fb047a76c65
JakubKrol <contact@itdotfocus.com>
2024-02-10 18:17:18 +0100

    Connection with ChromaDb.

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
-import {sayHelloPrompt} from "./prompts/say-hello.prompt";
-import {helloExamplesPrompt} from "./prompts/examples/hello.examples.prompt";
+import {API_KEY} from "./config";
+import {Chroma} from '@langchain/community/vectorstores/chroma';
+import {OpenAIEmbeddings} from '@langchain/openai';
 
 (async () => {
 
-    const text = await helloExamplesPrompt.format({
-        name: 'Jakub',
-    });
+    const vectorStore = await Chroma.fromExistingCollection(
+        new OpenAIEmbeddings({
+            openAIApiKey: API_KEY,
+        }),
+        {
+            collectionName: 'mistrz-ai-test',
+            url: ' http://localhost:8000',
+        },
+    );
 
-    console.log(text);
+    console.log(await vectorStore.index.listCollections());
 
 })();
