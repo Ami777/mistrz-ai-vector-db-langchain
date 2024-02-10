85ea3f4058e2073454b1120141916c5600749193
JakubKrol <contact@itdotfocus.com>
2024-02-10 18:25:00 +0100

    Load PDF with document loader.

--- index.ts
+++ index.ts
@@ -1,19 +1,29 @@
-import {API_KEY} from "./config";
-import {Chroma} from '@langchain/community/vectorstores/chroma';
-import {OpenAIEmbeddings} from '@langchain/openai';
+// import {API_KEY} from "./config";
+// import {Chroma} from '@langchain/community/vectorstores/chroma';
+// import {OpenAIEmbeddings} from '@langchain/openai';
+
+import {PDFLoader} from "langchain/document_loaders/fs/pdf";
 
 (async () => {
 
-    const vectorStore = await Chroma.fromExistingCollection(
-        new OpenAIEmbeddings({
-            openAIApiKey: API_KEY,
-        }),
-        {
-            collectionName: 'mistrz-ai-test',
-            url: ' http://localhost:8000',
-        },
-    );
+    // const vectorStore = await Chroma.fromExistingCollection(
+    //     new OpenAIEmbeddings({
+    //         openAIApiKey: API_KEY,
+    //     }),
+    //     {
+    //         collectionName: 'mistrz-ai-test',
+    //         url: ' http://localhost:8000',
+    //     },
+    // );
+    //
+    // console.log(await vectorStore.index.listCollections());
 
-    console.log(await vectorStore.index.listCollections());
+    const loader = new PDFLoader('./data/MegaK etap 2 Node js.pdf', {
+        parsedItemSeparator: ' ',
+    });
+
+    const docs = await loader.loadAndSplit();
+
+    console.log(docs[0]);
 
 })();
