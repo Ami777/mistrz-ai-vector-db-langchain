9fdc6cbb66fa4fc51f0fd2196c3acc7a1b449eaa
JakubKrol <contact@itdotfocus.com>
2024-02-10 18:27:17 +0100

    Add documents to vector DB.

--- index.ts
+++ index.ts
@@ -1,22 +1,19 @@
-// import {API_KEY} from "./config";
-// import {Chroma} from '@langchain/community/vectorstores/chroma';
-// import {OpenAIEmbeddings} from '@langchain/openai';
-
+import {API_KEY} from "./config";
+import {Chroma} from '@langchain/community/vectorstores/chroma';
+import {OpenAIEmbeddings} from '@langchain/openai';
 import {PDFLoader} from "langchain/document_loaders/fs/pdf";
 
 (async () => {
 
-    // const vectorStore = await Chroma.fromExistingCollection(
-    //     new OpenAIEmbeddings({
-    //         openAIApiKey: API_KEY,
-    //     }),
-    //     {
-    //         collectionName: 'mistrz-ai-test',
-    //         url: ' http://localhost:8000',
-    //     },
-    // );
-    //
-    // console.log(await vectorStore.index.listCollections());
+    const vectorStore = await Chroma.fromExistingCollection(
+        new OpenAIEmbeddings({
+            openAIApiKey: API_KEY,
+        }),
+        {
+            collectionName: 'mistrz-ai-test',
+            url: ' http://localhost:8000',
+        },
+    );
 
     const loader = new PDFLoader('./data/MegaK etap 2 Node js.pdf', {
         parsedItemSeparator: ' ',
@@ -24,6 +21,8 @@ import {PDFLoader} from "langchain/document_loaders/fs/pdf";
 
     const docs = await loader.loadAndSplit();
 
-    console.log(docs[0]);
+    const res = await vectorStore.addDocuments(docs);
+
+    console.log(res);
 
 })();
